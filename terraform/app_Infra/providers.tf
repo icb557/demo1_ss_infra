2e05521f247be17d7d05d5d3b78261799b224150
Isac Cortés Buitrago <cortesbuitragoisac@gmail.com>
2025-06-12 01:57:05 -0500

    FAD-53 feat: add remote terraform state bucket

--- terraform/app_Infra/providers.tf
+++ terraform/app_Infra/providers.tf
@@ -1,9 +1,38 @@
+resource "aws_s3_bucket" "terraform_state_bucket" {
+  bucket = "terraform-state-bucket-ss"
+}
+
+resource "aws_s3_bucket_versioning" "versioning_terraform_state_bucket" {
+  bucket = aws_s3_bucket.terraform_state_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name         = "terraform-state-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
 terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "terraform-state-bucket-ss"
+    key          = "terraform.tfstate"
+    region       = "us-east-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
