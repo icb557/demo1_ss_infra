f1f54085618783bf1f72e5b56d9d310137c70f32
Isac Cortés Buitrago <cortesbuitragoisac@gmail.com>
2025-06-12 19:07:18 -0500

    FAD-53-task Refactor: use lock file instead of dynamo db table for locking

--- terraform/app_Infra/providers.tf
+++ terraform/app_Infra/providers.tf
@@ -1,23 +1,13 @@
-resource "aws_s3_bucket" "terraform_state_bucket" {
-  bucket = "terraform-state-bucket-ss"
-}
+# resource "aws_s3_bucket" "terraform_state_bucket" {
+#   bucket = "terraform-state-bucket-ss"
+# }
 
-resource "aws_s3_bucket_versioning" "versioning_terraform_state_bucket" {
-  bucket = aws_s3_bucket.terraform_state_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_state_lock" {
-  name         = "terraform-state-lock"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+# resource "aws_s3_bucket_versioning" "versioning_terraform_state_bucket" {
+#   bucket = aws_s3_bucket.terraform_state_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
 terraform {
   required_providers {
